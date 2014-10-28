c81ca7e16e0564f0c11fa999e23dcc72eb06fb84
Lincoln Yu <lincoln.yu@xpsolutions.com>
2014-10-28 11:45:00 +1000

    Missing check-in of INotifyPropertyChanging.

---
+++ Apollo/Apollo/INotifyPropertyChanging.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Apollo
+{
+    /// <summary>
+    ///  claiming to be able to notify before a property is changed
+    /// </summary>
+    public interface INotifyPropertyChanging
+    {
+        #region Events
+
+        event PropertyChangedEventHandler PropertyChanging;
+
+        #endregion
+    }
+}
